99d3e8256c5a23c0689d8fb9f6b40e0975dd48ab
Adithya GV <160870256+adithyagv@users.noreply.github.com>
2025-08-10 23:57:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ dotenv.config({ path: path.join(__dirname, ".env") });
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/shopify", shopifyRoutes);
